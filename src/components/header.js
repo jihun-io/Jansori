bbb08c272cb09e7059922f9211c0429eafd95958
김지훈 <kim@jihun.io>
2024-09-15 03:44:26 +0900

    제목 스타일 변경

--- src/components/header.js
+++ src/components/header.js
@@ -9,8 +9,13 @@ export default function Header({ className }) {
 
   return (
     <header className={classNames(defaultClasses, className)}>
-      <Link href="/">
-        <h1>잔소리 키오스크</h1>
+      <Link
+        href="/"
+        className=" text-supernova-500 hover:text-supernova-700 transition-colors"
+      >
+        <h1 className="[text-shadow:_1.5px_3px_0.5px_rgb(24_24_24_/_90%)]">
+          잔소리 키오스크
+        </h1>
       </Link>
     </header>
   );
